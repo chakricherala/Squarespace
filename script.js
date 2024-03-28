3f1821c72e5539bb8cbc6ece118f3f3d7c555d68
Chakradhar Cherala <68188570+chakricherala@users.noreply.github.com>
2024-03-28 13:34:30 +0530

    gsap

---
+++ script.js
@@ -0,0 +1,44 @@
+let sq = gsap.timeline()
+
+sq.from(".navbar img, .navbar .middle,.navbar .right",{
+    y: -100,
+    duration: 1,
+    delay: 1,
+    stagger: 0.2
+})
+
+sq.from("h1",{
+    x: -100,
+    opacity: 0,
+    stagger: 0.2
+})
+
+sq.from(".bn",{
+    y: 100,
+    opacity: 0,
+    stagger: 0.2
+})
+
+sq.from("h2",{
+    y: 100,
+    opacity: 0,
+    stagger: 0.2
+})
+
+sq.from("#main img",{
+    duration: 1,
+    opacity: 0,
+    scale: 0
+})
+
+sq.from("h3",{
+    opacity: 0,
+    stagger: 0.2
+})
+
+sq.to("h3",{
+    y: 20,
+    duration: 1,
+    repeat: -1,
+    yoyo:true
+})
